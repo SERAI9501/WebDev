e1825746f437601278be0dcea997bac02ed561db
sist102 <sist102@DESKTOP-06VP6JK>
2025-03-13 09:11:47 +0900

    2025.3.13

---
+++ JSPLastProject/src/main/java/com/sist/vo/QnABoardVO.java
@@ -0,0 +1,10 @@
+package com.sist.vo;
+import java.util.*;
+
+import lombok.Data;
+@Data
+public class QnABoardVO {
+	private int no,hit,group_id,group_step,group_tab;
+	private String name,id,subject,content,pwd,anok,dbday;
+	private Date regdate;
+}
